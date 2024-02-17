9e764a23c5ca925d0aa94d48b47e425526424f9c
Abbas-Davodkhani <107187833+Abbas-Davodkhani@users.noreply.github.com>
2024-02-17 21:35:12 +0330

    very simple navbar

---
+++ SimpleNavbar/navbar/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="nav-background">
+        <header>
+          <h2 className="nav-logo">
+            React JS
+          </h2>
+          <ul className="nav-menu">
+            <li className="nav-link"><a href="#">Home</a></li>
+            <li className="nav-link"><a href="#">About</a></li>
+            <li className="nav-link"><a href="#">Contact</a></li>
+            <li className="nav-link"><a href="#">Shop</a></li>
+          </ul>
+        </header>
+    </div>
+  );
+}
+
+export default App;
